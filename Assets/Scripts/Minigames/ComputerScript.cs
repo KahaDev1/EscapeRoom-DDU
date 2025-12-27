1c10718a90287bd7b45b2ff874f53c0d595cc5ec
Karl Anton <capsicremeDevs@gmail.com>
2025-12-27 13:05:29 +0100

    Add computer overlay functionality

--- Assets/Scripts/Minigames/ComputerScript.cs
+++ Assets/Scripts/Minigames/ComputerScript.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ComputerScript : MonoBehaviour
+, IPointerEnterHandler
+, IPointerExitHandler
 {
 
     Player player;
 
     [SerializeField] LayerMask overlayLayer;
     [SerializeField] Collider2D background;
+    bool hoveringOverlay;
 
     void Start()
     {
         player = FindAnyObjectByType<Player>();
     }
 
-
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
-            Collider2D detectedCollider = Physics2D.OverlapPoint(mousePos, overlayLayer);
-            if (detectedCollider != background)
+            if (!hoveringOverlay)
             {
                 player.minigameOverlayActive = false;
                 gameObject.SetActive(false);
             }
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hoveringOverlay = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hoveringOverlay = false;
     }
 }
